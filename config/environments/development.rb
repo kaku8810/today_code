76b85f0f8fa57efef693618d23dded72531c6907
haya8ya <kakukakunakakukun@gmail.com>
2020-02-10 15:04:53 +0900

    開発環境でメール送信成功

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # default url
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['EMAIL'],
+    :password => ENV['EMAILPASS'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
