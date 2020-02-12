fbde5c503fae144b34a55bc9b1409b0c0227e33f
haya8ya <kakukakunakakukun@gmail.com>
2020-02-12 13:22:34 +0900

    ユーザー名の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-  def configure_permitted_parameters
-    added_attrs = [ :email, :user_name, :password, :password_confirmation ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
   
 end
