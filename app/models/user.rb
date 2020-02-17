79829e47ad441817d9deabf2b184644abcd9f3ec
haya8ya <kakukakunakakukun@gmail.com>
2020-02-17 15:23:30 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
