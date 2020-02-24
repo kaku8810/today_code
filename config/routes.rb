9dfe6c4514b5aa3625e3f1f99db85acb8b0e97a6
haya8ya <kakukakunakakukun@gmail.com>
2020-02-24 14:21:29 +0900

    twitterログインのエラー解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top_page#top'
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks", registrations: 'users/registrations'}
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations'}
   resources :users, only: [:show]
   
 end
