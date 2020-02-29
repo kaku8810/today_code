df2f9bebcf5ddb3cc7f170a9f2f23f53c80b589c
haya8ya <kakukakunakakukun@gmail.com>
2020-02-29 16:19:41 +0900

    layout修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
-class TweetsController < ApplicationController
+class tweetsController < ApplicationController
+  before_action :authenticate_user! 
+
+  def new
+  end
+
+  def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      flash[:success] = "tweet created!"
+      redirect_to root_url
+    else
+      render 'top_page/top'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'top_page#top'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations'}
   resources :users, only: [:show]
+  resources :tweets,          only: [:new, :create, :destroy]
   
 end
