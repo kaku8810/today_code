ee00eee67c99c454dcdbe8deda68cec8528910f9
haya8ya <kakukakunakakukun@gmail.com>
2020-03-10 22:24:07 +0900

    show_followの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tweets = @user.tweets.page(params[:page]).per(10)
   end
+
+  def following
+    @title = "フォロー"
+    @user  = User.find(params[:id])
+    @users = @user.following.page(params[:page]).per(10)
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user  = User.find(params[:id])
+    @users = @user.followers.page(params[:page]).per(10)
+    render 'show_follow'
+  end
 end
