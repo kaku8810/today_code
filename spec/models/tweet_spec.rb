be45d3b7e0e55b7cd9fa3aec04eb045a7fde6392
haya8ya <kakukakunakakukun@gmail.com>
2020-03-20 13:57:09 +0900

    Tweetモデルのテスト作成

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  it 'user_idとcontentがあれば有効' do
-    user = FactoryBot.create(:user)
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  it 'user_idとaがあれば有効' do
+    user = @user
     tweet = user.tweets.build(
       content: '佐々木希',
       user_id: 1
     )
     expect(tweet).to be_valid
+  end
+
+  it 'contentがなければ無効' do
+    tweet = Tweet.new(content: nil)
+    tweet.valid?
+    expect(tweet.errors[:content]).to include("を入力してください")
+  end
+
+  it 'contentが140字を超えると無効' do
+    tweet = Tweet.new(content: 'a' * 141)
+    tweet.valid?
+    expect(tweet.errors[:content]).to include("は140文字以内で入力してください")
   end
 end
