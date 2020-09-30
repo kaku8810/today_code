de55d37b5ef321aad7bbfa7be69fd1973a922b0c
haya8ya <kakukakunakakukun@gmail.com>
2020-09-30 16:20:18 +0900

    routes追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   root 'top_page#top'
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations'}
+
   resources :users, only: [:show] do
     get :following, :followers, on: :member
   end
+
   resources :tweets, only: [:index, :new, :create, :destroy] do
     get 'home', on: :collection
+    resources :likes, only: [:create, :destroy]
   end
+
   resources :relationships,       only: [:create, :destroy]
   
 end
