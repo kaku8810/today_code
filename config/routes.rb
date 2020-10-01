d86b82118f7b90ebe9eca1ca40e976c8e081bd41
haya8ya <kakukakunakakukun@gmail.com>
2020-10-01 18:12:27 +0900

    ランキング機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :tweets, only: [:index, :new, :create, :destroy] do
     get 'home', on: :collection
-    #resources :likes, only: [:create, :destroy]
+    get 'ranks', on: :collection
   end
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
