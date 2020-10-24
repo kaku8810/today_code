e051932814e1647cab190d2a40537ae1b6aa7a11
haya8ya <kakukakunakakukun@gmail.com>
2020-10-24 23:47:56 +0900

    likeモデルtest骨格追加

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  describe 'バリデーション' do
+    it 'user_idとtweet_idがあれば有効' do
+      
+    end
+
+    it 'user_idがないと無効' do
+      
+    end
+
+    it 'tweet_idがないと無効' do
+      
+    end
+  end
 end
