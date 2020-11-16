0379ba463ae19123c2a6c1e720315617b7f8d720
haya8ya <kakukakunakakukun@gmail.com>
2020-11-16 23:35:40 +0900

    空行削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   before_action :tweet_params
   def create
     Like.create(user_id: current_user.id, tweet_id: params[:id])
-    
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy
-    
   end
 
   def tweet_params
